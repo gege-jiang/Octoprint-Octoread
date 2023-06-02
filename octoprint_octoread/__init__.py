3ac203515d8eecc07a633aa2d7cb4f1d6fb3e338
gege-jiang <gegejiang22@gmail.com>
2023-06-01 19:28:43 -0700

    initial commit

--- octoprint_octoread/__init__.py
+++ octoprint_octoread/__init__.py
@@ -5,6 +5,6 @@ class HelloWorldPlugin(octoprint.plugin.StartupPlugin,
     def on_after_startup(self):
         self._logger.info("Hello World!")
 
-# __plugin_implementation__ = HelloWorldPlugin()
+__plugin_implementation__ = HelloWorldPlugin()
 __plugin_name__ = "Octoread"
 __plugin_pythoncompat__ = ">=3.7,<4"
